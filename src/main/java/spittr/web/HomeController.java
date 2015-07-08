76ca1589d8d67e6b7ab26b17f3c3935f07275fa6
kkolcz <kkolcz@power.com.pl>
2015-07-08 10:41:27 +0200

    routing at class level

--- src/main/java/spittr/web/HomeController.java
+++ src/main/java/spittr/web/HomeController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping({"/","/homepage"})
 public class HomeController {
-@RequestMapping(value="/", method=GET)
     public String home() {
         return "home";
     }
