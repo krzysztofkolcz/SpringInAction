6a6cbcb6158e47219e02d4d50e0ba05e066c2921
Krzysztof Kołcz <kkolcz@power.com.pl>
2016-01-19 09:07:01 +0100

    successfull compile and run

--- chapter02/javaconfig/src/main/java/soundsystem/Main.java
+++ chapter02/javaconfig/src/main/java/soundsystem/Main.java
@@ -1,10 +1,12 @@
 package soundsystem;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 public class Main {
   public static void main(String[] args) throws Exception {
     ApplicationContext context = new AnnotationConfigApplicationContext(CDPlayerConfig.class);
     CDPlayer player = context.getBean(CDPlayer.class);
-    context.close();
+      player.play();
   }
 }
