4db12c40d7d102f3e2dde3d65788396c038bf363
Krzysztof Kołcz <kkolcz@power.com.pl>
2016-01-21 18:16:09 +0100

    Spittle controller, repository interface, pojo and test added

--- chapter05_tmp01/spitt/src/test/java/spittr/web/HomeControllerTest.java
+++ chapter05_tmp01/spitt/src/test/java/spittr/web/HomeControllerTest.java
@@ -12,6 +12,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import spittr.web.HomeController;
+import spittr.web.SpittleController;
+import spittr.Spittle;
+import spittr.data.SpittleRepository;
 
 import org.springframework.web.servlet.view.InternalResourceView;
 
@@ -28,5 +31,26 @@ public class HomeControllerTest {
         HomeController controller = new HomeController();
         MockMvc mockMvc = standaloneSetup(controller).build();
         mockMvc.perform(get("/")).andExpect(view().name("home"));
+    }
+
+    private List<Spittle> createSpittleList(int count) {
+        List<Spittle> spittles = new ArrayList<Spittle>();
+        for (int i=0; i < count; i++) {
+            spittles.add(new Spittle("Spittle " + i, new Date()));
+        }
+        return spittles;
+    }
+
+    @Test
+    public void shouldShowRecentSpittles() throws Exception {
+        List<Spittle> expectedSpittles = createSpittleList(20);
+        SpittleRepository mockRepository = mock(SpittleRepository.class);
+        when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedSpittles);
+        SpittleController controller = new SpittleController(mockRepository);
+        MockMvc mockMvc = standaloneSetup(controller).setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp")).build();
+        mockMvc.perform(get("/spittles"))
+        .andExpect(view().name("spittles"))
+        .andExpect(model().attributeExists("spittleList"))
+        .andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
     }
 }
