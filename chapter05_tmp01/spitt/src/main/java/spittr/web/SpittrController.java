7061ab79ab27e4dbe30fd5fe20526b80e008e488
krzysztof <krzysztof.kolcz@gmail.com>
2016-01-22 22:47:01 +0100

    jsp interpreted error resolved - problem - web.xml

---
+++ chapter05_tmp01/spitt/src/main/java/spittr/web/SpittrController.java
@@ -0,0 +1,21 @@
+package spittr.web;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import spittr.Spittle;
+import spittr.data.SpittleRepository;
+
+@Controller
+@RequestMapping("/spittr")
+public class SpittrController {
+    @RequestMapping(method=RequestMethod.GET)
+    public String spittr(Model model) {
+        model.addAttribute("test","yo yo yo");
+        return "spittr";
+    }
+}
