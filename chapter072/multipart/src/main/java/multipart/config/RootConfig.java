b9c15ace98edcee5139de524fffc718048e7d4b3
krzysztof <krzysztof.kolcz@gmail.com>
2016-01-31 22:52:26 +0100

    chapter 7.2

---
+++ chapter072/multipart/src/main/java/multipart/config/RootConfig.java
@@ -0,0 +1,11 @@
+package multipart.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages={"multipart"}, excludeFilters={ @Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class) })
+public class RootConfig {
+}
---
+++ chapter072/multipart/target/multipart/WEB-INF/home.jsp
@@ -0,0 +1,11 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+    <head>
+        <title>Multipart</title>
+        <link rel="stylesheet" type="text/css" href="<c:url value="/resources/style.css" />" >
+    </head>
+    <body>
+        <h1>Welcome to Multipart</h1>
+    </body>
+</html>
