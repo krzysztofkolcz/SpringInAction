76af689b36ddb031e9c1b96491ed5251a4c9d26a
krzysztof <krzysztof.kolcz@gmail.com>
2016-02-01 23:13:35 +0100

    websystique tutorial

--- chapter072/multipart/src/main/java/multipart/config/MultipartWebAppInitializer.java
+++ chapter072/multipart/src/main/java/multipart/config/MultipartWebAppInitializer.java
@@ -1,5 +1,7 @@
 package multipart.config;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
 
 public class MultipartWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -9,10 +11,26 @@ public class MultipartWebAppInitializer extends AbstractAnnotationConfigDispatch
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[] { RootConfig.class };
+        /* return new Class<?>[] { RootConfig.class }; */
+        return new Class<?>[] { WebConfig.class };
     }
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[] { WebConfig.class };
+        //return new Class<?>[] { WebConfig.class };
+        return null;
     }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(getMultipartConfigElement());
+    }
+
+    private MultipartConfigElement getMultipartConfigElement() {
+        MultipartConfigElement multipartConfigElement = new MultipartConfigElement( LOCATION, MAX_FILE_SIZE, MAX_REQUEST_SIZE, FILE_SIZE_THRESHOLD);
+        return multipartConfigElement;
+    }
+    private static final String LOCATION = "/tmp/"; // Temporary location where files will be stored
+    private static final long MAX_FILE_SIZE = 5242880; // 5MB : Max file size.  // Beyond that size spring will throw exception.
+    private static final long MAX_REQUEST_SIZE = 20971520; // 20MB : Total request size containing Multi part.
+    private static final int FILE_SIZE_THRESHOLD = 0; // Size threshold after which files will be written to disk
 }
--- chapter072/multipart/src/main/java/multipart/config/WebConfig.java
+++ chapter072/multipart/src/main/java/multipart/config/WebConfig.java
@@ -8,14 +8,22 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "multipart.controller" })
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    @Bean(name = "multipartResolver")
+    public StandardServletMultipartResolver resolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+  /*
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -25,11 +33,22 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         resolver.setExposeContextBeansAsAttributes(true);
         return resolver;
     }
+    */
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        registry.viewResolver(viewResolver);
+    }
 
+    /*
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+    */
 
     @Bean
     public MessageSource messageSource(){
@@ -37,4 +56,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         messageSource.setBasename("messages");
         return messageSource;
     }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations( "/static/");
+    }
+
 }
